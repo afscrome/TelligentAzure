71df813bc7b3e14f63e91703f66ac998c1c1ea6f
Alex Crome <afscrome@users.noreply.github.com>
2015-11-24 20:51:14 +0000

    Initial project structure

---
+++ Filestorage/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Azure Blob Filestorage provider for Telligent")]
+[assembly: AssemblyProduct("Tellgient extensions for Azure")]
+[assembly: AssemblyCopyright("Copyright © Alex Crome 2015")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("")]
