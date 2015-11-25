79e546b8ca9a3e466eacda76b5fc25f004b21388
Alex Crome <afscrome@users.noreply.github.com>
2015-11-25 17:54:58 +0000

    Initial Provider

---
+++ Filestorage/AzureBlobFileReference.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.IO;
+using Telligent.Evolution.Extensibility.Storage.Version1;
+using System.Linq;
+using Telligent.Common.Diagnostics.Tracing;
+
+namespace AlexCrome.Telligent.Azure.Filestorage
+{
+    public class AzureBlobFileReference : ICentralizedFile
+    {
+        private readonly CloudBlob _blob;
+
+
+        public AzureBlobFileReference(CloudBlob blob, string fileStoreKey)
+        {
+            _blob = blob;
+            FileStoreKey = fileStoreKey;
+        }
+
+        public int ContentLength => (int)_blob.Properties.Length;
+        public string FileName => _blob.Uri.Segments[_blob.Uri.Segments.Length - 1];
+        public string FileStoreKey { get; }
+        public string Path
+        {
+            get
+            {
+                if (_blob.Parent == null)
+                    return string.Empty;
+
+                var containerSegmentCount = _blob.Container.Uri.Segments.Length;
+                var pathSegments = _blob.Parent.Uri.Segments
+                    .Skip(containerSegmentCount)
+                    .Select(x => x.TrimEnd('/'))
+                    .ToArray();
+
+                return CentralizedFileStorage.MakePath(pathSegments);
+            }
+        }
+
+        public string GetDownloadUrl() => _blob.Uri.ToString();
+
+        public Stream OpenReadStream() => new TracedStream(_blob.OpenRead(), $"[cfs] ReadStream '{FileStoreKey}' '{Path}' '{FileName}'");
+
+        private class TracedStream : Stream
+        {
+            private readonly Stream _inner;
+            private readonly TracePoint _tracePoint;
+
+            public TracedStream(Stream inner, string tracePointName)
+            {
+                _inner = inner;
+                _tracePoint = new TracePoint(tracePointName);
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                _inner.Dispose();
+                _tracePoint.Dispose();
+            }
+
+            public override bool CanRead => _inner.CanRead;
+            public override bool CanSeek => _inner.CanSeek;
+            public override bool CanWrite => _inner.CanWrite;
+            public override long Length => _inner.Length;
+            public override void Flush() => _inner.Flush();
+            public override int Read(byte[] buffer, int offset, int count) => _inner.Read(buffer, offset, count);
+            public override long Seek(long offset, SeekOrigin origin) => _inner.Seek(offset, origin);
+            public override void SetLength(long value) => _inner.SetLength(value);
+            public override void Write(byte[] buffer, int offset, int count) => _inner.Write(buffer, offset, count);
+            public override long Position
+            {
+                get { return _inner.Position; }
+                set { _inner.Position = value; }
+            }
+
+        }
+    }
+}
---
+++ Filestorage/AzureBlobFilestorageProvider.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using Telligent.Evolution.Extensibility.Storage.Version1;
+using System;
+using System.Collections;
+using System.Net;
+using Telligent.Common.Diagnostics.Tracing;
+
+namespace AlexCrome.Telligent.Azure.Filestorage
+{
+
+    //TODO: IPersistentUrlGeneratingFileStorageProvider, IEventEnabledCentralizedFileStorageProvider, IHttpAsyncRenderableCentralizedFileStorageProvider 
+    public class AzureBlobFilestorageProvider : ICentralizedFileStorageProvider
+    {
+        private CloudBlobContainer _container;
+
+        public string FileStoreKey { get; private set; }
+
+
+        public void Initialize(string fileStoreKey, XmlNode configurationNode)
+        {
+            FileStoreKey = fileStoreKey;
+
+            //TODO: share account & client across provider instances to reuse the buffer pool
+            var account = CloudStorageAccount.DevelopmentStorageAccount;
+            var client = account.CreateCloudBlobClient();
+            _container = client.GetContainerReference(MakeSafeContainerName(fileStoreKey));
+            _container.CreateIfNotExists(BlobContainerPublicAccessType.Blob);
+
+            var defaultOptions = client.DefaultRequestOptions;
+            defaultOptions.MaximumExecutionTime = TimeSpan.FromSeconds(3);
+            defaultOptions.ServerTimeout = TimeSpan.FromSeconds(1);
+        }
+
+        public ICentralizedFile AddFile(string path, string fileName, Stream contentStream, bool ensureUniqueFileName)
+        {
+            if (ensureUniqueFileName)
+                fileName = CentralizedFileStorage.GetUniqueFileName(this, path, fileName);
+
+            return AddUpdateFile(path, fileName, contentStream);
+        }
+
+        public void AddPath(string path)
+        {
+            //Azure doesn't support creating a directory without content
+
+            //AddUpdateFile(path, ".placeholder", Stream.Null);
+        }
+
+        public ICentralizedFile AddUpdateFile(string path, string fileName, Stream contentStream)
+        {
+            using (new TracePoint($"[cfs] AddUpdate '{FileStoreKey}' '{path}' '{fileName}'"))
+            {
+                var blob = GetBlob(path, fileName);
+
+                blob.UploadFromStream(contentStream);
+
+                return new AzureBlobFileReference(blob, FileStoreKey);
+            }
+        }
+
+        public void Delete() => Delete(string.Empty);
+
+        public void Delete(string path)
+        {
+            using (new TracePoint($"[cfs] Delete '{FileStoreKey}' '{path}'"))
+            {
+                foreach (var blob in ListChildren(path, PathSearchOption.AllPaths).OfType<CloudBlob>())
+                {
+                    blob.DeleteIfExists();
+                }
+            }
+        }
+
+        public void Delete(string path, string fileName)
+        {
+            using (new TracePoint($"[cfs] Delete '{FileStoreKey}' '{path}' '{fileName}'"))
+            {
+                var blob = GetBlob(path, fileName);
+                blob.DeleteIfExists();
+            }
+        }
+
+        public ICentralizedFile GetFile(string path, string fileName)
+        {
+            using (new TracePoint($"[cfs] GetFile '{FileStoreKey}' '{path}' '{fileName}'"))
+            {
+                var blob = GetBlob(path, fileName);
+
+                if (blob.Exists())
+                    return new AzureBlobFileReference(blob, FileStoreKey);
+
+                return null;
+            }
+        }
+
+        public IEnumerable<ICentralizedFile> GetFiles(PathSearchOption searchOption)
+            => GetFiles(string.Empty, searchOption);
+
+        public IEnumerable<ICentralizedFile> GetFiles(string path, PathSearchOption searchOption)
+        {
+            using (new TracePoint($"[cfs] GetFiles '{FileStoreKey}' '{path}'"))
+            {
+                return ListChildren(path, searchOption)
+                .OfType<CloudBlockBlob>()
+                .Select(x => new AzureBlobFileReference(x, FileStoreKey));
+            }
+        }
+
+
+        public IEnumerable<string> GetPaths()
+            => GetPaths(string.Empty);
+
+        public IEnumerable<string> GetPaths(string path)
+        {
+            using (new TracePoint($"[cfs] GetPaths '{FileStoreKey}' '{path}'"))
+            {
+                //TODO: Not entirely sure this is correct
+                return ListChildren(path, PathSearchOption.TopLevelPathOnly)
+                .OfType<CloudBlobDirectory>()
+                .Select(x => x.Prefix);
+            }
+        }
+
+
+        public static string MakeSafeContainerName(string fileStoreKey)
+        {
+            var containerName = fileStoreKey.Replace("-", "--")
+                                .Replace(".", "-");
+            return WebUtility.UrlEncode(containerName);
+        }
+
+        private CloudBlockBlob GetBlob(string path, string fileName)
+        {
+            var azurePath = ConvertCfsPathToAzurePath(path);
+            return _container.GetBlockBlobReference(azurePath + "/" + fileName);
+        }
+
+        private IEnumerable<IListBlobItem> ListChildren(string path, PathSearchOption searchOption)
+        {
+            bool includeSubDirectories = searchOption == PathSearchOption.AllPaths;
+
+            IEnumerable<IListBlobItem> blobs;
+
+            if (string.IsNullOrEmpty(path))
+            {
+                blobs = _container.ListBlobs(useFlatBlobListing: includeSubDirectories);
+            }
+            else
+            {
+                var azurePath = ConvertCfsPathToAzurePath(path);
+                blobs = _container.GetDirectoryReference(azurePath)
+                                .ListBlobs(includeSubDirectories);
+            }
+
+            return new NotFoundHandlingEnumerable(blobs);
+        }
+
+
+
+        private string ConvertCfsPathToAzurePath(string cfsPath)
+            => cfsPath?.Replace('.', '/').Trim('.');
+
+
+        private class NotFoundHandlingEnumerable : IEnumerable<IListBlobItem>
+        {
+            private readonly IEnumerable<IListBlobItem> _original;
+            public NotFoundHandlingEnumerable(IEnumerable<IListBlobItem> original)
+            {
+                _original = original;
+            }
+
+            public IEnumerator<IListBlobItem> GetEnumerator() => new NotFoundHandlingEnumerator(_original.GetEnumerator());
+
+            IEnumerator IEnumerable.GetEnumerator() => new NotFoundHandlingEnumerator(_original.GetEnumerator());
+
+            private class NotFoundHandlingEnumerator : IEnumerator<IListBlobItem>
+            {
+                private IEnumerator<IListBlobItem> _original;
+                public NotFoundHandlingEnumerator(IEnumerator<IListBlobItem> original)
+                {
+                    _original = original;
+                }
+
+                public object Current => _original.Current;
+
+                IListBlobItem IEnumerator<IListBlobItem>.Current => _original.Current;
+
+                public void Dispose() => _original.Dispose();
+                public bool MoveNext()
+                {
+                    try
+                    {
+                        return _original.MoveNext();
+                    }
+                    catch (StorageException ex) when (ex.RequestInformation.HttpStatusCode == 404)
+                    {
+                        return false;
+                    }
+                }
+
+                public void Reset() => _original.Reset();
+            }
+        }
+
+
+    }
+}
